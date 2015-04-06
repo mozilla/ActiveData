d3b7b631c6d28b60626d89233482d70d3d98aa1a
Kyle Lahnakoski <klahnakoski@mozilla.com>
2015-04-06 14:27:38 -0400

    add test to cover variable extraction failure

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -10,12 +10,14 @@
 
 from __future__ import unicode_literals
 from __future__ import division
+from pyLibrary.queries.expressions import get_all_vars
 from pyLibrary.queries.filters import simplify_esfilter, where2esfilter
 
 from pyLibrary.testing.fuzzytestcase import FuzzyTestCase
 
 
 class TestExpressions(FuzzyTestCase):
+
     def test_range_packing(self):
         where = {"and": [
             {"gt": {"a": 20}},
@@ -24,3 +26,8 @@ class TestExpressions(FuzzyTestCase):
 
         result = simplify_esfilter(where2esfilter(where))
         self.assertEqual(result, {"range": {"a": {"gt": 20, "lt": 40}}})
+
+    def test_value_not_a_variable(self):
+        result = get_all_vars({"eq":{"result.test":"/XMLHttpRequest/send-entity-body-document.htm"}})
+        expected = set(["result.test"])
+        self.assertEqual(result, expected, "expecting the one and only variable")

8dca6acc33e440964b18b5b0ee029fc9e02f1e64
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-01-14 16:31:03 -0500

    fix regression

--- pyLibrary/queries/es09/util.py
+++ pyLibrary/queries/es09/util.py
@@ -37,7 +37,7 @@ DEBUG = False
 def post(es, es_query, limit):
     post_result = None
     try:
-        post_result = es.search(elasticsearch.scrub(es_query))
+        post_result = es.search(es_query)
 
         for facetName, f in post_result.facets.items():
             if f._type == "statistical":

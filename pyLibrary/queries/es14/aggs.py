a7a867794ec64061e413e02ea90566d8c8937aed
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-02-10 16:17:54 -0500

    fixes

--- pyLibrary/queries/es14/aggs.py
+++ pyLibrary/queries/es14/aggs.py
@@ -175,11 +175,11 @@ def es_aggsop(es, frum, query):
     decoders = get_decoders_by_depth(query)
     start = 0
 
-    vars_ = qb_expression(query.where).vars()
+    vars_ = query.where.vars()
     map_ = {v: frum[v].abs_name for v in vars_}
 
     #<TERRIBLE SECTION> THIS IS WHERE WE WEAVE THE where CLAUSE WITH nested
-    split_where = split_expression_by_depth(qb_expression(query.where), schema=frum, map_=map_)
+    split_where = split_expression_by_depth(query.where, schema=frum, map_=map_)
 
     if len(split_field(frum.name)) > 1:
         if any(split_where[2::]):

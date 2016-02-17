96cd9b6e4669d3bd71d78fa0e6db9ca839e67674
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-02-17 10:17:30 -0500

    actually use the travis config file

--- tests/travis/run.sh
+++ tests/travis/run.sh
@@ -2,5 +2,7 @@ export PYTHONPATH=.
 
 python active_data/app.py --settings=tests/travis/app.json &
 sleep 5
+
+export TEST_CONFIG=tests/travis/app.json
 python -m unittest discover tests --failfast
 exit

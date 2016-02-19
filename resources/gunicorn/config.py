929a5e0e17e9b05dd170b36f52552eb4f67848ea
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-02-19 10:13:44 -0500

    try this

--- resources/gunicorn/config.py
+++ resources/gunicorn/config.py
@@ -4,8 +4,8 @@ backlog = 64
 workers = 1
 timeout = 24*60*60
 
-# keyfile = "/home/ec2-user/.ssh/activedata.allizom.org.key"
-# certfile = "/home/ec2-user/.ssh/activedata_allizom_org.crt"
-# ca_certs = "/home/ec2-user/.ssh/DigiCertCA.crt"
+keyfile = "/home/ec2-user/.ssh/activedata.allizom.org.key"
+certfile = "/home/ec2-user/.ssh/activedata_allizom_org.crt"
+ca_certs = "/home/ec2-user/.ssh/DigiCertCA.crt"
 
 

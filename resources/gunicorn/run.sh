cafee8ef47fe9677e1ee865bd81d2f5c55f132dd
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-02-19 08:06:48 -0500

    try this

--- resources/gunicorn/run.sh
+++ resources/gunicorn/run.sh
@@ -6,14 +6,16 @@ export HOME=/home/ec2-user
 cd ~/ActiveData
 
 
-# MAGICAL GUNICORN PATH MINI-LANGUAGE (True to most *nix help, the most important is left out)
-#
 # usage: gunicorn [OPTIONS] [APP_MODULE]
 #
-# APP_MODULE - is in the form <MODULE_PATH> ":" <METHOD_NAME> <OPTIONAL_PARAMETERS>
-#              where
-#              <MODULE_PATH> is a dot delimited path to the module
-#              <METHOD_NAME> name of method in the module
-#              <OPTIONAL_PARAMETERS> when concatenated with <METHOD_NAME> results in the ?Python? syntax method call.
+# [APP_MODULE] IS A POORLY DOCUMENTED MINI-LANGUAGE:
+#
+# <APP_MODULE> = <MODULE_PATH> ":" <METHOD_NAME> <OPTIONAL_PARAMETERS>
+#
+# where
+#
+# <MODULE_PATH> = is a dot delimited path to the module
+# <METHOD_NAME> = name of method in the module
+# <OPTIONAL_PARAMETERS> = when concatenated with <METHOD_NAME> results in the ?Python? syntax method call.
 
 /usr/local/bin/gunicorn --pythonpath . --config resources/gunicorn/config.py 'active_data.app:main(settings="resources/config/staging_settings.json")'

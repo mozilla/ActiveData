fdc24685e797be88dd3f9d6c193e7c239ea5ada2
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-02-18 20:36:37 -0500

    try this

--- resources/gunicorn/run.sh
+++ resources/gunicorn/run.sh
@@ -4,4 +4,4 @@ sudo -i
 export PYTHONPATH=.
 export HOME=/home/ec2-user
 cd ~/ActiveData
-/usr/local/bin/gunicorn --config resources/gunicorn/config.py active_data:app:main(settings=
+/usr/local/bin/gunicorn --pythonpath . --config resources/gunicorn/config.py 'active_data.app:main(settings="resources/config/staging_settings.json")'

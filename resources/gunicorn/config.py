721e4889ec299a58c7e7e9b838f2a9b8a4d8a02e
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-02-18 20:46:24 -0500

    try this

--- resources/gunicorn/config.py
+++ resources/gunicorn/config.py
@@ -1,7 +1,7 @@
 bind = "127.0.0.1:80"
 
 backlog = 64
-workers = 32
+workers = 1
 
 keyfile = "/home/ec2-user/.ssh/activedata.allizom.org.key"
 certfile = "/home/ec2-user/.ssh/activedata_allizom_org.crt"

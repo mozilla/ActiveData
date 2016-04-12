b49e73fc9d804fd1af53c79cb06528dfbba8adf6
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-04-12 11:35:01 -0400

    gunicorn log change

--- resources/config/gunicorn.py
+++ resources/config/gunicorn.py
@@ -6,3 +6,17 @@ timeout = 24*60*60
 
 accesslog = "/logs/gunicorn_access.log"
 errorlog = "/logs/gunicorn_error.log"
+
+access_log_format = '{' \
+                    '"remote_addr": "%(h)s",' \
+                    '"remote_user": "%(u)s",' \
+                    '"timestamp": $msec,' \
+                    '"local_time": "%(t)s",' \
+                    '"request": "%(r)s",' \
+                    '"status": "%(s)s",' \
+                    '"bytes_sent": %(b)s,' \
+                    '"referer": "%(f)s",' \
+                    '"user_agent": "%(a)s",' \
+                    '"upstream_request_time": %(T)s,' \
+                    '"process_id": %(p)s' \
+                    '}'

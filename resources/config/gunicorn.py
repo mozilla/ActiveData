c94180a2a772dc3ce2860f7cbcd1d50c8b5f12a1
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-04-12 11:51:43 -0400

    better config

--- resources/config/gunicorn.py
+++ resources/config/gunicorn.py
@@ -10,13 +10,12 @@ errorlog = "/logs/gunicorn_error.log"
 access_log_format = '{' \
                     '"remote_addr": "%(h)s",' \
                     '"remote_user": "%(u)s",' \
-                    '"timestamp": $msec,' \
                     '"local_time": "%(t)s",' \
                     '"request": "%(r)s",' \
                     '"status": "%(s)s",' \
                     '"bytes_sent": %(b)s,' \
                     '"referer": "%(f)s",' \
                     '"user_agent": "%(a)s",' \
-                    '"upstream_request_time": %(T)s,' \
+                    '"upstream_request_time": %(D)s,' \
                     '"process_id": %(p)s' \
                     '}'

1306f7317fe938f4c6b149120832b5c27708b565
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-05-19 12:23:37 -0400

    all tests pass, except the one needing metadata

--- tests/test_query_normalization.py
+++ tests/test_query_normalization.py
@@ -1,3 +1,16 @@
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Author: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+
+from __future__ import division
+from __future__ import unicode_literals
+
 from pyLibrary.queries.query import _normalize_edge, _normalize_select
 from pyLibrary.testing.fuzzytestcase import FuzzyTestCase
 

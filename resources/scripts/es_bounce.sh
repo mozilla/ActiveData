e435951057a9c0490d3549db415a8d70c89ebcda
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-08-13 22:01:37 -0400

    add parameters for a more abstract shard balancer

--- resources/scripts/es_bounce.sh
+++ resources/scripts/es_bounce.sh
@@ -5,7 +5,7 @@ curl -XPUT http://localhost:9200/_cluster/settings -d "{\"persistent\": {\"clust
 
 curl -XPUT http://localhost:9200/_cluster/settings -d "{\"persistent\": {\"cluster.routing.allocation.balance.primary\": 1}}"
 curl -XPUT http://localhost:9200/_cluster/settings -d "{\"persistent\": {\"cluster.routing.allocation.enable\": \"all\"}}"
-
+curl -XPUT http://localhost:9200/_cluster/settings -d '{"transient": {"cluster.routing.allocation.disk.watermark.low": "80%"}}'
 
 curl -XPUT -d '{"index": {"number_of_replicas": 1}}'  http://localhost:9200/_settings
 

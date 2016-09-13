1794c0b6ae300af3d30077e9ff9d4d90fd26f7d7
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-09-13 11:36:38 -0400

    wrap dicts

--- pyLibrary/queries/es14/aggs.py
+++ pyLibrary/queries/es14/aggs.py
@@ -39,7 +39,7 @@ def get_decoders_by_depth(query):
     """
     schema = query.frum
     output = DictList()
-    for e in coalesce(query.edges, query.groupby, []):
+    for e in wrap(coalesce(query.edges, query.groupby, [])):
         if e.value != None:
             e = e.copy()
             e.value = jx_expression(e.value)

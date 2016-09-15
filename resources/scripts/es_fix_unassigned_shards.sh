f4a23838c429312fa18357eb3524fe68ef91da5f
Kyle Lahnakoski <klahnakoski@mozilla.com>
2016-09-15 08:04:14 -0400

    fix script

--- resources/scripts/es_fix_unassigned_shards.sh
+++ resources/scripts/es_fix_unassigned_shards.sh
@@ -5,5 +5,5 @@ cd ~/ActiveData
 git pull origin better-balance
 export PYTHONPATH=.
 
-nohup python27 resources/scripts/es_fix_unassigned_shards.py --settings=resources/config/fix_unassigned_shards.json
+nohup python27 resources/scripts/es_fix_unassigned_shards.py --settings=resources/config/fix_unassigned_shards.json &
 tail -n200 -f ./results/fix_unassigned_shards.log

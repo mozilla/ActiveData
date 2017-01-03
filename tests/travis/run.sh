85147569264f6698f2bd04e274432cf52a5bf91f
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-01-03 15:02:05 -0500

    try this

--- tests/travis/run.sh
+++ tests/travis/run.sh
@@ -4,5 +4,5 @@ python active_data/app.py --settings=tests/travis/app.json &
 sleep 5
 
 export TEST_CONFIG=tests/travis/app.json
-python -m unittest discover tests/test_load_and_save_queries.py --failfast
+python -m unittest discover tests.test_load_and_save_queries --failfast
 exit

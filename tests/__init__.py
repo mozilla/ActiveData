bf58da22063a03f3506c539a691f8ba5e7ac33d6
maggienj <maggie.tech.nj@gmail.com>
2017-06-05 18:38:06 -0400

    UnitTest: TestSetOps.test_number Status: Passed Method: Not sure if this is the right method. But, tried to identify the datatype of the value and added a datatype ( type ) property while inserting the data (index) in ES. This appears to be working for this unit test scenario.

--- tests/__init__.py
+++ tests/__init__.py
@@ -171,9 +171,23 @@ class ESUtils(object):
             container.add_alias(_settings.index)
 
             # INSERT DATA
-            container.extend([
-                {"value": v} for v in subtest.data
-            ])
+
+            for v in subtest.data:
+                currDataType = type(v.v) # just for debugging
+                if v != None:
+                    if isinstance(v.v,long):
+                        vType="long"
+                        container.extend([{"value": v, "type": "\"" + vType + "\""}])
+                    if isinstance(v.v,str) or isinstance(v.v,unicode):
+                        vType="string"
+                        container.extend([{"value": v, "type": "\"" + vType + "\""}])
+                    if isinstance(v.v,int):
+                        vType="double"
+                        container.extend([{"value": v, "type": "\"" + vType + "\""}])
+                else:
+                    vType == "string"
+                    container.extend([{"value": v, "type": "\"" + vType + "\""}])
+
             container.flush()
             # ENSURE query POINTS TO CONTAINER
             frum = subtest.query["from"]

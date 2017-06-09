3d873a91582b83ea83ce3f55c0fd679799a251a5
maggienj <maggie.tech.nj@gmail.com>
2017-06-09 07:37:21 -0400

    UnitTest: changes related to getting this TestSetOps.test_number to pass + size gt zero changes are clubbed in this commit. reverted limit size back to 0 in aggs.py

--- pyLibrary/queries/es14/aggs.py
+++ pyLibrary/queries/es14/aggs.py
@@ -331,7 +331,7 @@ def es_aggsop(es, frum, query):
     if not es_query:
         es_query = wrap({"query": {"match_all": {}}})
 
-    es_query.size = 1
+    es_query.size = 0
 
     with Timer("ES query time") as es_duration:
         result = es09.util.post(es, es_query, query.limit)

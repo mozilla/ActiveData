27e7bd931197f889faa305358bb5e3a718ef7e9b
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-08-01 18:35:03 -0400

    new coverage post-etl

--- active_data/jobs/codecoverage.py
+++ active_data/jobs/codecoverage.py
@@ -10,238 +10,117 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
-from mo_dots import coalesce, wrap, unwrap
+from jx_python import jx
+from mo_dots import coalesce, wrap
 from mo_logs import Log
 from mo_logs import constants
 from mo_logs import startup
-from mo_math import MIN
 from mo_math import UNION
 from mo_threads import Thread, Signal, Queue, THREAD_STOP
 
-from mo_dots.datas import leaves
 from mo_times.dates import Date, unicode2Date
 from mo_times.timer import Timer
 from pyLibrary.env import http, elasticsearch
-from jx_python import jx
 
 DEBUG = False
 NUM_THREAD = 4
 
 
-def process_batch(todo, coverage_index, coverage_summary_index, settings, please_stop):
-    for not_summarized in todo:
+def process_batch(todo_queue, revision, coverage_index, coverage_summary_index, settings, please_stop):
+    """
+    :param todo: list of files to process as a single block 
+    :param coverage_index: 
+    :param coverage_summary_index: 
+    :param settings: 
+    :param please_stop: 
+    :return: 
+    """
+
+    for todo in todo_queue:
         if please_stop:
-            return True
+            return
 
-        # IS THERE MORE THAN ONE COVERAGE FILE FOR THIS REVISION?
-        # Log.note("Find dups for file {{file}}", file=not_summarized.source.file.name)
-        # dups = http.post_json(settings.url, json={
-        #     "from": "coverage",
-        #     "select": [
-        #         # THIS IS FAR FROM GOOD, WE WANT THE LATEST ETL ID, BUT etl.source.id IS ONLY A SUB-ID
-        #         # WE SHOULD BE ABLE TO GET THE max OF A TUPLE
-        #         {"name": "max_id", "value": "etl.source.id", "aggregate": "max"},
-        #         {"name": "min_id", "value": "etl.source.id", "aggregate": "min"}
-        #     ],
-        #     "where": {"and": [
-        #         {"missing": "source.method.name"},
-        #         {"neq": {"source.file.total_covered": 0}},
-        #         {"eq": {
-        #             "source.file.name": not_summarized.source.file.name,
-        #             "build.revision12": not_summarized.build.revision12
-        #         }},
-        #     ]},
-        #     "groupby": [
-        #         "test.suite",
-        #         "test.chunk",
-        #         "test.url",
-        #         "test.name"
-        #     ],
-        #     "limit": 100000,
-        #     "format": "list"
-        # })
+        # WHAT HAVE WE SUMMARIED ALREADY?
+        Log.note("Coverage for revision {{revision}}:\n{{files}}", revision=revision, files=todo.source.file.name)
 
-        # dups_found = False
-        # for d in dups.data:
-        #     if d.max_id != d.min_id:
-        #         # FIND ALL INDEXES
-        #         dups_found = True
-        #         Log.note(
-        #             "removing dups {{details|json}}",
-        #             details={"and": [
-        #                 {"not": {"term": {"etl.source.id": int(d.max_id)}}},
-        #                 {"and":[
-        #                     {"term": {k: v}} for k, v in leaves({"test": d.test})
-        #                 ]},
-        #                 {"term": {"source.file.name": not_summarized.source.file.name}},
-        #                 {"term": {"build.revision12": not_summarized.build.revision12}}
-        #             ]}
-        #         )
-        #
-        #         coverage_index.delete_record({"and": [
-        #             {"not": {"term": {"etl.source.id": int(d.max_id)}}},
-        #             {"and": [
-        #                 {"term": {k: v}} for k, v in leaves({"test": d.test})
-        #             ]},
-        #             {"term": {"test.url": d.test.url}},
-        #             {"term": {"source.file.name": not_summarized.source.file.name}},
-        #             {"term": {"build.revision12": not_summarized.build.revision12}}
-        #         ]})
-        # if dups_found:
-        #     continue
-
-        # LIST ALL TESTS THAT COVER THIS FILE, AND THE LINES COVERED
-        test_count = http.post_json(settings.url, json={
-            "from": "coverage.source.file.covered",
+        coverage_summary_records = http.post_json(settings.url, json={
+            "from": "coverage-summary",
+            "select": [{"name": "count", "value": "etl.num_source_records", "aggregate": "sum"}],
+            "edges": ["source.file.name"],
             "where": {"and": [
-                {"missing": "source.method.name"},
-                {"neq": {"source.file.total_covered": 0}},
-                {"eq": {
-                    "source.file.name": not_summarized.source.file.name,
-                    "build.revision12": not_summarized.build.revision12
-                }},
+                {"eq": {"build.revision12": revision}},
+                {"in": {"source.file.name": todo.source.file.name}}
             ]},
-            "groupby": [
-                "test.suite",
-                "test.chunk",
-                "test.url",
-                "test.name",
-                "line"
-            ],
             "limit": 100000,
             "format": "list"
-        })
+        }).data
+        existing_count_summary = {t.source.file.name: t.count for t in coverage_summary_records}
 
-        all_tests_covering_file = UNION(test_count.data.get("test"))
-        num_tests = len(all_tests_covering_file)
-        max_siblings = num_tests - 1
-        Log.note(
-            "{{filename}} rev {{revision}} is covered by {{num}} tests",
-            filename=not_summarized.source.file.name,
-            num=num_tests,
-            revision=not_summarized.build.revision12
-        )
-        line_summary = list(
-            (k, unwrap(wrap(list(v)).get("test")))
-            for k, v in jx.groupby(test_count.data, keys="line")
-        )
+        refresh_required = [
+            rec.source.file.name
+            for rec in todo
+            if rec.source.file.name and existing_count_summary.get(rec.source.file.name) != rec.count
+        ]
 
-        test_urls = set(wrap(list(all_tests_covering_file)).url) - {None}
-        test_suites = set(wrap(list(all_tests_covering_file)).suite) - {None}
-
-        if test_urls:
-            if test_suites:
-                Log.error("expecting test.url or test suite, but not both at this time")
-            test_filter = {"in": {"test.url": test_urls}}
-        elif test_suites:
-            test_filter = {"in": {"test.suite": test_suites}}
-        else:
-            Log.error("expecting some tests")
-
-        # PULL THE RAW RECORD FOR MODIFICATION
-        file_level_coverage_records = http.post_json(settings.url, json={
+        # PULL AN EXAMPLE
+        coverage_example = http.post_json(settings.url, json={
             "from": "coverage",
             "where": {"and": [
                 {"missing": "source.method.name"},
                 {"neq": {"source.file.total_covered": 0}},
-                test_filter,
-                {"eq": {
-                    "source.file.name": not_summarized.source.file.name,
-                    "build.revision12": not_summarized.build.revision12
-                }}
+                {"eq": {"build.revision12": revision}},
+                {"in": {"source.file.name": refresh_required}}
+            ]},
+            "limit": 1,
+            "format": "list"
+        }).data
+
+        # PULL THE COVERAGE RECORDS
+        coverage_records = http.post_json(settings.url, json={
+            "from": "coverage",
+            "select": "source.file",
+            "where": {"and": [
+                {"missing": "source.method.name"},
+                {"neq": {"source.file.total_covered": 0}},
+                {"eq": {"build.revision12": revision}},
+                {"in": {"source.file.name": refresh_required}}
             ]},
             "limit": 100000,
             "format": "list"
-        })
+        }).data
 
-        for test_name in all_tests_covering_file:
-            siblings = [len(test_names)-1 for g, test_names in line_summary if test_name in test_names]
-            min_siblings = MIN(siblings)
-            coverage_candidates = wrap([row for row in file_level_coverage_records.data if row.test == test_name])
-            if coverage_candidates:
-                # MORE THAN ONE COVERAGE CANDIDATE CAN HAPPEN WHEN THE SAME TEST IS IN TWO DIFFERENT CHUNKS OF THE SAME SUITE
-                for coverage_record in coverage_candidates:
-                    coverage_record.source.file.max_test_siblings = max_siblings
-                    coverage_record.source.file.min_line_siblings = min_siblings  # PLACEHOLDER TO INDICATE DONE
-                    coverage_record.source.file.score = (max_siblings - min_siblings) / (max_siblings + min_siblings + 1)
-            else:
-                example = http.post_json(settings.url, json={
-                    "from": "coverage",
-                    "where": {"and": [
-                        {"eq": {k: v for k, v in leaves({"test": test_name})}},
-                        {"eq": {
-                            "source.file.name": not_summarized.source.file.name,
-                            "build.revision12": not_summarized.build.revision12
-                        }}
-                    ]},
-                    "limit": 1,
-                    "format": "list"
-                })
+        coverage_summaries = []
+        for g, file_level_coverage_records in jx.groupby(coverage_records, "name"):
+            source_file_name = g["name"]
 
-                Log.warning(
-                    "{{test|quote}} rev {{revision}} appears to have no coverage for {{file|quote}}!\n{{example|json|indent}}",
-                    test=test_name,
-                    file=not_summarized.source.file.name,
-                    revision=not_summarized.build.revision12,
-                    example=example.data[0]
-                )
-
-        bad_example = [d for d in file_level_coverage_records.data if d["source.file.min_line_siblings"] == None]
-        if bad_example:
-            Log.warning("expecting all records to have summary. Example:\n{{example}}", example=bad_example[0])
-
-        # NOW THAT source.file.min_line_siblings IS SET, WE CAN RE-SAVE THESE TO coverage
-        rows = [{"id": d._id, "value": d} for d in file_level_coverage_records.data]
-        coverage_index.extend(rows)
-
-        all_test_summary = []
-        for g, records in jx.groupby(file_level_coverage_records.data, "source.file.name"):
-            g = unwrap(g)
-            cov = UNION(records.source.file.covered)
-            uncov = UNION(records.source.file.uncovered)
+            cov = UNION(file_level_coverage_records.covered)
+            uncov = UNION(file_level_coverage_records.uncovered) - cov
             coverage = {
-                "_id": "|".join([records[0].build.revision12, g["source.file.name"]]),  # SOMETHING UNIQUE, IN CASE WE RECALCULATE
                 "source": {
-                    "language": records[0].source.language,
+                    "language": coverage_example[0].source.language,
                     "file": {
-                        "name": g["source.file.name"],
+                        "name": source_file_name,
                         "is_file": True,
-                        "covered": jx.sort(cov, "line"),
+                        "covered": jx.sort(cov),
                         "uncovered": jx.sort(uncov),
                         "total_covered": len(cov),
                         "total_uncovered": len(uncov),
                         "min_line_siblings": 0  # PLACEHOLDER TO INDICATE DONE
                     }
                 },
-                "build": records[0].build,
-                "repo": records[0].repo,
-                "etl": {"timestamp": Date.now()}
+                "build": coverage_example[0].build,
+                "repo": coverage_example[0].repo,
+                "etl": {
+                    "timestamp": Date.now(),
+                    "num_source_records": len(file_level_coverage_records)  # RECORD NUMBER OF RECORDS USED TO COMPOSE THIS; IF THERE ARE MORE IN THE FUTURE, RECALC
+                }
             }
-            all_test_summary.append(coverage)
-
-        summary_rows = [{"id": d["_id"], "value": d} for d in all_test_summary]
-        coverage_summary_index.extend(summary_rows)
-
-        if DEBUG:
-            coverage_index.refresh()
-            todo = http.post_json(settings.url, json={
-                "from": "coverage",
-                "where": {"and": [
-                    {"neq": {"source.file.total_covered": 0}},
-                    {"missing": "source.method.name"},
-                    {"missing": "source.file.min_line_siblings"},  # MISSING PLACEHOLDER TO INDICATE DONE
-                    {"eq": {"source.file.name": not_summarized.source.file.name}},
-                    {"eq": {"build.revision12": not_summarized.build.revision12}}
-                ]},
-                "format": "list",
-                "limit": 10
+            coverage_summaries.append({
+                "id": "|".join([revision, source_file_name]),  # SOMETHING UNIQUE, IN CASE WE RECALCULATE
+                "value": coverage
             })
-            if todo.data:
-                Log.warning(
-                    "Failure to update {{revision}}, {{file}}",
-                    revision=not_summarized.build.revision12,
-                    file=not_summarized.source.file.name
-                )
+
+        coverage_summary_index.extend(coverage_summaries)
 
 
 def loop(source, coverage_summary_index, settings, please_stop):
@@ -256,54 +135,63 @@ def loop(source, coverage_summary_index, settings, please_stop):
         candidates = jx.sort(candidates, {".": "desc"})
 
         for index_name in candidates:
+            if please_stop:
+                return
+
             coverage_index = elasticsearch.Index(index=index_name, read_only=False, kwargs=source)
             push_date_filter = unicode2Date(coverage_index.settings.index[-15::], elasticsearch.INDEX_DATE_FORMAT)
 
-            while not please_stop:
-                # IDENTIFY NEW WORK
-                with Timer("Pulling work from index {{index}}", param={"index":index_name}):
-                    coverage_index.refresh()
+            # IDENTIFY NEW WORK
+            with Timer("Pulling work from index {{index}}", param={"index": index_name}):
+                revisions = http.post_json(settings.url, json={
+                    "from": "coverage",
+                    "groupby": ["build.revision12", "repo.push.date"],
+                    "where": {"and": [
+                        {"gte": {"repo.push.date": push_date_filter}}
+                    ]},
+                    "format": "list",
+                    "sort": "repo.push.date",
+                    "limit": 10000
+                }).data
 
+                for rev in revisions.build.revision12:
                     todo = http.post_json(settings.url, json={
                         "from": "coverage",
-                        "groupby": ["source.file.name", "build.revision12"],
+                        "groupby": ["source.file.name"],
                         "where": {"and": [
+                            {"eq": {"build.revision12": rev}},
                             {"neq": {"source.file.total_covered": 0}},
-                            {"missing": "source.method.name"},
-                            {"missing": "source.file.min_line_siblings"},  # MISSING MEAN WORK IS NOT DONE
-                            {"gte": {"repo.push.date": push_date_filter}}
+                            {"missing": "source.method.name"}
                         ]},
                         "format": "list",
-                        "limit": coalesce(settings.batch_size, 100)
+                        "limit": 100000
                     })
 
-                if not todo.data:
-                    break
+                    queue = Queue("pending source files to review")
+                    queue.extend(_groupby_size(todo.data, size=10000))
 
-                queue = Queue("pending source files to review")
-                queue.extend(todo.data[0:coalesce(settings.batch_size, 100):])
+                    num_threads = coalesce(settings.threads, NUM_THREAD)
+                    Log.note("Launch {{num}} threads", num=num_threads)
+                    threads = [
+                        Thread.run(
+                            "processor" + unicode(i),
+                            process_batch,
+                            queue,
+                            rev,
+                            coverage_index,
+                            coverage_summary_index,
+                            settings,
+                            please_stop=please_stop
+                        )
+                        for i in range(num_threads)
+                    ]
 
-                num_threads = coalesce(settings.threads, NUM_THREAD)
-                Log.note("Launch {{num}} threads", num=num_threads)
-                threads = [
-                    Thread.run(
-                        "processor" + unicode(i),
-                        process_batch,
-                        queue,
-                        coverage_index,
-                        coverage_summary_index,
-                        settings,
-                        please_stop=please_stop
-                    )
-                    for i in range(num_threads)
-                ]
+                    # ADD STOP MESSAGE
+                    queue.add(THREAD_STOP)
 
-                # ADD STOP MESSAGE
-                queue.add(THREAD_STOP)
-
-                # WAIT FOR THEM TO COMPLETE
-                for t in threads:
-                    t.join()
+                    # WAIT FOR THEM TO COMPLETE
+                    for t in threads:
+                        t.join()
         return
 
     except Exception, e:
@@ -311,6 +199,20 @@ def loop(source, coverage_summary_index, settings, please_stop):
     finally:
         Log.note("Processing loop is done")
         please_stop.go()
+
+
+def _groupby_size(items, size):
+    acc = 0
+    output = []
+    for i in items:
+        if acc + i.count > size:
+            yield wrap(output)
+            acc = 0
+            output = []
+        acc += i.count
+        output.append(i)
+    if output:
+        yield wrap(output)
 
 
 def main():

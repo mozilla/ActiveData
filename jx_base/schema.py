46b06318f532296b7570448f4467c0f0386e1448
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-08-13 11:55:21 -0400

    revert lost methods

--- jx_base/schema.py
+++ jx_base/schema.py
@@ -13,8 +13,10 @@ from __future__ import unicode_literals
 
 from copy import copy
 
-from mo_dots import join_field, split_field, Null
+from mo_dots import join_field, split_field, Null, startswith_field, concat_field, set_default
 from mo_logs import Log
+
+from jx_base import STRUCT
 
 
 def _indexer(columns, query_path):
@@ -42,13 +44,12 @@ class Schema(object):
         """
         table_path = split_field(table_name)
         self.table = table_path[0]  # USED AS AN EXPLICIT STATEMENT OF PERSPECTIVE IN THE DATABASE
-        self.query_path = join_field(table_path[1:])
+        self.query_path = join_field(table_path[1:])  # TODO: REPLACE WITH THE nested_path ARRAY
         self._columns = copy(columns)
 
         lookup = self.lookup = _indexer(columns, self.query_path)
         if self.query_path != ".":
-            from jx_python import _index
-            alternate = _index(columns, ".")
+            alternate = _indexer(columns, ".")
             for k,v in alternate.items():
                 lookup.setdefault(k, v)
 
@@ -68,6 +69,41 @@ class Schema(object):
         :return: NAME OF column
         """
         return column.names[self.query_path]
+
+    def leaves(self, name):
+        """
+        RETURN LEAVES OF GIVEN PATH NAME
+        :param name: 
+        :return: 
+        """
+        full_name = concat_field(self.query_path, name)
+        return [
+            c
+            for k, cs in self.lookup.items()
+            if startswith_field(k, full_name)
+            for c in cs
+            if c.type not in STRUCT
+        ]
+
+    def map_to_es(self):
+        """
+        RETURN A MAP FROM THE NAME SPACE TO THE es_column NAME
+        """
+        full_name = self.query_path
+        return set_default(
+            {
+                c.names[full_name]: c.es_column
+                for k, cs in self.lookup.items()
+                # if startswith_field(k, full_name)
+                for c in cs if c.type not in STRUCT
+            },
+            {
+                c.names["."]: c.es_column
+                for k, cs in self.lookup.items()
+                # if startswith_field(k, full_name)
+                for c in cs if c.type not in STRUCT
+            }
+        )
 
     @property
     def columns(self):

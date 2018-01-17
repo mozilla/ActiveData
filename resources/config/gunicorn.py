ef116895f3a5937f83512f4fa5aec68923963c6d
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-01-16 23:13:24 -0500

    try this

--- resources/config/gunicorn.py
+++ resources/config/gunicorn.py
@@ -6,18 +6,21 @@ timeout = 24*60*60
 
 capture_output = True
 accesslog = "/data1/logs/gunicorn_access.log"
-errorlog = "/data1/logs/gunicorn_error.log"
+errorlog = "-"
+# errorlog = "/data1/logs/gunicorn_error.log"
 logfile = "/data1/logs/gunicorn_debug.log"
 
-access_log_format = '{' \
-                    '"remote_addr": "%(h)s",' \
-                    '"remote_user": "%(u)s",' \
-                    '"local_time": "%(t)s",' \
-                    '"request": "%(r)s",' \
-                    '"status": "%(s)s",' \
-                    '"bytes_sent": %(b)s,' \
-                    '"referer": "%(f)s",' \
-                    '"user_agent": "%(a)s",' \
-                    '"upstream_request_time": %(D)s,' \
-                    '"process_id": "%(p)s"' \
-                    '}'
+access_log_format = (
+    '{' 
+    '"remote_addr": "%(h)s",' 
+    '"remote_user": "%(u)s",' 
+    '"local_time": "%(t)s",' 
+    '"request": "%(r)s",' 
+    '"status": "%(s)s",' 
+    '"bytes_sent": %(b)s,' 
+    '"referer": "%(f)s",' 
+    '"user_agent": "%(a)s",' 
+    '"upstream_request_time": %(D)s,' 
+    '"process_id": "%(p)s"' 
+    '}'
+)

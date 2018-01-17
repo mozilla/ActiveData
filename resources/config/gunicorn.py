fe1e3c7a1af4d30c5b6626bfcd06bb89f4f78249
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-01-16 22:49:38 -0500

    try this

--- resources/config/gunicorn.py
+++ resources/config/gunicorn.py
@@ -4,6 +4,7 @@ backlog = 64
 workers = 20
 timeout = 24*60*60
 
+capture_output = True
 accesslog = "/data1/logs/gunicorn_access.log"
 errorlog = "/data1/logs/gunicorn_error.log"
 logfile = "/data1/logs/gunicorn_debug.log"

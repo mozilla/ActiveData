b3b8648904e52eea9c07a76917b09907bfceb6ce
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-06-18 10:32:09 -0400

    faster ingestion

--- resources/scripts/es_set_refresh_interval.sh
+++ resources/scripts/es_set_refresh_interval.sh
@@ -2,3 +2,8 @@ curl -XPUT localhost:9200/unittest/_settings -d '{"index" : {"refresh_interval" 
 curl -XPUT localhost:9200/coverage/_settings -d '{"index" : {"refresh_interval" : "1h"} }'  -H "Content-Type: application/json"
 
 curl -XPUT localhost:9200/unittest/_settings -d '{"index" : {"refresh_interval" : "5m"} }'  -H "Content-Type: application/json"
+
+
+curl -XPUT localhost:9200/_settings -d '{"index.translog.durability": "async"}'  -H "Content-Type: application/json"
+curl -XPUT localhost:9200/_settings -d '{"index.translog.flush_threshold_size": "1000mb"}'  -H "Content-Type: application/json"
+curl -XPUT localhost:9200/_settings -d '{"index.translog.sync_interval": "60s"}'  -H "Content-Type: application/json"

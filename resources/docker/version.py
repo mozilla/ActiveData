75acf0229c1640094566a85b263cc2eb9f24d9ed
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-07-23 12:47:39 -0400

    always use REPO_TAG

--- resources/docker/version.py
+++ resources/docker/version.py
@@ -1,6 +1,5 @@
 import os
 
-from mo_dots import coalesce
 from mo_files import File
 from mo_future import text_type
 from mo_json import value2json
@@ -11,7 +10,7 @@ print("Uses *.dockerfile environment variables to write a verion.json file")
 File("version.json").write(value2json(
     {
         "source": os.environ.get('REPO_URL'),
-        "version": coalesce(os.environ.get('REPO_TAG'), os.environ.get('REPO_BRANCH')),
+        "version": os.environ.get('REPO_TAG'),
         "commit": get_git_revision(),
         "build": os.environ.get('BUILD_URL')
     },

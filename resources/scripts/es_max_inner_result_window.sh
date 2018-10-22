61eeb0e417407d1059d4537bfa3ebd2c7e6b6359
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-10-21 21:41:36 -0400

    fix max_inner_result_window

--- resources/scripts/es_max_inner_result_window.sh
+++ resources/scripts/es_max_inner_result_window.sh
@@ -1,6 +1,6 @@
 
 
-curl -XPUT 'localhost:9200/treeherder/_settings' -d '{"index" : {"max_inner_result_window": 100000}}'  -H "Content-Type: application/json"
+curl -XPUT 'localhost:9200/task/_settings' -d '{"index" : {"max_inner_result_window": 100000}}'  -H "Content-Type: application/json"
 
 
 

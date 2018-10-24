db7f3902cbfa5a0ed7f5381f6413a4988b639026
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-10-24 05:34:54 -0400

    more scripts

--- resources/scripts/es_add_alias.sh
+++ resources/scripts/es_add_alias.sh
@@ -1,3 +1,6 @@
 
 
 curl -XPOST http://54.148.242.195:9200/_aliases -d "{\"actions\":[{\"add\":{\"index\":\"coverage20180930_000000\",\"alias\":\"coverage\"}}]}"  -H "Content-Type: application/json"
+
+
+curl -XPOST http://localhost:9200/_aliases -d "{\"actions\":[{\"add\":{\"index\":\"repo20181023_165516\",\"alias\":\"repo\"}}]}"  -H "Content-Type: application/json"

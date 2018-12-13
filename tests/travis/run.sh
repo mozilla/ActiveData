652bbe4b730f6b6b5cc1f895d06a06a87f3aa56c
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-12-13 13:39:55 -0500

    faster test for failure

--- tests/travis/run.sh
+++ tests/travis/run.sh
@@ -4,8 +4,8 @@ python active_data/app.py --settings=tests/travis/app.json &
 sleep 5
 
 export TEST_CONFIG=tests/travis/app.json
-python -m unittest discover tests --failfast
+#python -m unittest discover tests --failfast
 #python -m unittest test_load_and_save_queries.TestLoadAndSaveQueries
-#python -m unittest tests.test_jx.test_expressions_w_set_ops.TestSetOps.test_left_and_right
+python -m unittest tests.test_jx.test_expressions_w_set_ops.TestSetOps.test_left_and_right
 
 exit
